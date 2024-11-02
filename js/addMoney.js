1d01ed96dfe0e5cdbe268d2a7d1ff08c0d569e6c
shawonvai25 <abdullahalshawon25@gmail.com>
2024-11-03 00:20:53 +0600

    practices add money function

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,9 +2,27 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault()
     
     const addMoney = document.getElementById('input-add-money').value;
-    console.log(addMoney);
+    const addMoneyNumber = parseFloat(addMoney);
 
-    const pinNumber = document.getAnimations('input-pin-number'). value;
-    console.log(pinNumber);
+    const pinNumber = document.getElementById('input-pin-number'). value;
+    
+    console.log(addMoney,pinNumber);
 
+    // wrong way to verify pin
+    if(pinNumber === '1234'){
+     const balance = document.getElementById('account-balance').innerText;
+     const balanceNumber = parseFloat(balance);
+
+     const newBalance = balanceNumber + addMoneyNumber;
+     
+     document.getElementById('account-balance').innerText = newBalance;
+
+     
+     document.getElementById('input-add-money').value = '';
+     document.getElementById('input-pin-number').value = '';
+
+    }
+    else{
+        alert('Failed to add money.')
+    }
 })
