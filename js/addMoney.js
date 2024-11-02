44c2524e702d1a71f92a44bebe2896757720672c
shawonvai25 <abdullahalshawon25@gmail.com>
2024-11-02 17:09:18 +0600

    added first commit

---
+++ js/addMoney.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault()
+    
+    const addMoney = document.getElementById('input-add-money').value;
+    console.log(addMoney);
+
+    const pinNumber = document.getAnimations('input-pin-number'). value;
+    console.log(pinNumber);
+
+})
